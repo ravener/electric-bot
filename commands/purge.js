50200e320f90c3560574d15b82c8ca84dcd88d38
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-02-12 09:02:01 -0900

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,30 @@
+exports.run = (client, message, args) => {
+
+const user = message.mentions.users.first();
+const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+if (!amount) return message.reply('Must specify an amount to delete!');
+if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+message.channel.fetchMessages({
+ limit: amount,
+}).then((messages) => {
+ if (user) {
+ const filterBy = user ? user.id : Client.user.id;
+ messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+ }
+ message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+ });
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "Moderator"
+};
+
+exports.help = {
+  name: "purge",
+  category: "Moderation",
+  description: "Purge X amount of messages",
+  usage: "purge @user 10"
+};
