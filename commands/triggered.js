752782729ffafb5de1a0946d2a5a315a41546149
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-02-26 14:13:50 -0900

    Create triggered.js

---
+++ commands/triggered.js
@@ -0,0 +1,23 @@
+exports.run = async(client, message, args, level) => {
+  const user = message.member.mentions.first() || message.author;
+try {
+  await message.channel.send(new Discord.Attachment(
+  await client.api.triggered(user.displayAvatarURL + "?size=2048"), "triggered.gif"));
+} catch(e) {
+ console.error(e);
+ }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "triggered",
+  category: "Fun",
+  description: "Somebody is triggered.",
+  usage: "triggered [@user]"
+};
