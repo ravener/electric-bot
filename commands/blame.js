0026c3a4840931c3c995d97885bd2fa753221739
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-05 21:34:54 +0300

    ops

--- commands/blame.js
+++ commands/blame.js
@@ -1,9 +1,9 @@
 exports.run = async(client, message, args, level) => {
 	if(!message.guild) {
-		await message.channel.send(await client.api.blame(args.length > 1 ? args.join(" ") : message.author.username)).catch(console.error);
+		await message.channel.send(await client.api.blame(args.length > 1 ? args.join(" ") : message.author.username))
 	} else {
 		 const user = message.mentions.members.first() ? message.mentions.members.first() : message.member;
-		 await message.channel.send(await client.api.blame(user.displayName)).catch(console.error);
+		 await message.channel.send(await client.api.blame(user.displayName))
 	}
 };
 
