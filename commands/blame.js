72ca3fa8fe39b6fa810066838c4a4de4521fa6b8
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-05 21:38:28 +0300

    im dumb

--- commands/blame.js
+++ commands/blame.js
@@ -1,9 +1,11 @@
+const Discord = require("discord.js");
+
 exports.run = async(client, message, args, level) => {
 	if(!message.guild) {
-		await message.channel.send(await client.api.blame(args.length > 1 ? args.join(" ") : message.author.username))
+		await message.channel.send(new Discord.Attachment(await client.api.blame(args.length > 1 ? args.join(" ") : message.author.username)))
 	} else {
 		 const user = message.mentions.members.first() ? message.mentions.members.first() : message.member;
-		 await message.channel.send(await client.api.blame(user.displayName))
+		 await message.channel.send(new Discord.Attachment(await client.api.blame(user.displayName)))
 	}
 };
 
