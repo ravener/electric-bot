b7cba68fa0d6816a6a97873e4d373a7e53214d31
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-05 21:40:58 +0300

    maybe fix

--- commands/blame.js
+++ commands/blame.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async(client, message, args, level) => {
 	if(!message.guild) {
-		await message.channel.send(new Discord.Attachment(await client.api.blame(args.length > 1 ? args.join(" ") : message.author.username)))
+		await message.channel.send(new Discord.Attachment(await client.api.blame(args.length > 0 ? args.join(" ") : message.author.username)))
 	} else {
 		 const user = message.mentions.members.first() ? message.mentions.members.first() : message.member;
 		 await message.channel.send(new Discord.Attachment(await client.api.blame(user.displayName)))
