3c1cb20c6d49a795d6c2e0657d76549c5b13d339
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-12 00:24:21 +0300

    dude

--- commands/copy.js
+++ commands/copy.js
@@ -2,10 +2,13 @@ const logger = require("../util/Logger.js");
 
 exports.run = (client, message, args, level) => {
 	if(!message.guild.me.permissions.has("MANAGE_EMOJIS")) return message.reply("I don't have manage emojis permissions.");
+	const argumentsPassed = args.join(" ");
 	if(args.length < 1) return message.reply("Please include an ID for the emoji to copy, in this format `copy emoji_id emoji_name`");
-	if(!parseInt(args.join(' ')[0])) return message.reply("That doesn't seem an ID to me");
-	if(!args.join(' ')[1]) return message.reply("Please add a name for the emoji, `copy emoji_id emoji_name`");
-	message.guild.createEmoji(`https://cdn.discordapp.com/emojis/${args.join(' ')[0]}`, args.join(' ')[1])
+	if(!parseInt(argumentsPassed[0])) return message.reply("That doesn't seem an ID to me");
+	if(!argumentsPassed[1]) return message.reply("Please add a name for the emoji, `copy emoji_id emoji_name`");
+	const id = argumentsPassed[0]
+	const name = argumentsPassed[1]
+	message.guild.createEmoji(`https://cdn.discordapp.com/emojis/${id}`, name)
 	.then(emoji => {
 		message.channel.send(`:thumbsup: copied emoji ${emoji.name} <$:{emoji.name}:${emoji.id}>`);
 	}).catch(e => {
