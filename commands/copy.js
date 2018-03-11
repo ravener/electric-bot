778f4a9ede70f4f76f3e8e957e41d9301f9cc355
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-12 00:17:55 +0300

    lol

--- commands/copy.js
+++ commands/copy.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args, level) => {
 	if(args.length < 1) return message.reply("Please include an ID for the emoji to copy, in this format `copy emoji_id emoji_name`");
 	if(!parseInt(args.join(' ')[0])) return message.reply("That doesn't seem an ID to me");
 	if(!args.join(' ')[1]) return message.reply("Please add a name for the emoji, `copy emoji_id emoji_name`");
-	message.guild.createEmoji(`https://cdn.discordapp.com/emojis/${id}`, args.join(' ')[1])
+	message.guild.createEmoji(`https://cdn.discordapp.com/emojis/${args.join(' ')[0]}`, args.join(' ')[1])
 	.then(emoji => {
 		message.channel.send(`:thumbsup: copied emoji ${emoji.name} <$:{emoji.name}:${emoji.id}>`);
 	}).catch(e => {
