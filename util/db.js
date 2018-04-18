946d0ed213153d36d908df499812faf4252c482b
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-04-19 02:02:22 +0300

    ffs

--- util/db.js
+++ util/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const tag = new mongoose.schema({
+const tag = new mongoose.Schema({
   name: { type: String, required: true },
   guild: { type: String, required: true },
   content: { type: String, required: true },
